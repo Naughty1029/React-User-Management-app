f0cf870867500c30abdee947a7eab34f45bcaacd
Naughty1029 <masahiro.watanabe02@gmail.com>
2022-05-20 15:47:26 +0900

    ユーザー取得

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,33 @@
 import { memo, VFC } from 'react';
+import { Center, Spinner, Wrap, WrapItem } from '@chakra-ui/react';
+import { UserCard } from '../organisms/user/UserCard';
+import { useAllUsers } from '../../hooks/useAllUsers';
+import { useEffect } from 'react';
 
 export const UserManagement: VFC = memo(() => {
-    return <p>ユーザー管理ページ</p>;
+    const { getUsers, users, loading } = useAllUsers();
+    useEffect(
+        () => {
+            getUsers();
+        },
+        // eslint-disable-next-line
+        [],
+    );
+    return (
+        <>
+            {loading ? (
+                <Center h="100vh">
+                    <Spinner />
+                </Center>
+            ) : (
+                <Wrap p={{ base: 4, md: 10 }}>
+                    {users.map((user) => (
+                        <WrapItem key={user.id} mx="auto">
+                            <UserCard imageUrl="https://source.unsplash.com/random" userName={user.username} fullName={user.name} />
+                        </WrapItem>
+                    ))}
+                </Wrap>
+            )}
+        </>
+    );
 });
