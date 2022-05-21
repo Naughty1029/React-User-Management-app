1a4bcafc395eec760de5d511c87837470332f9a0
渡邉雅広 <watanabemasahiro@masahiro.local>
2022-05-21 17:11:25 +0900

    ユーザー詳細モーダル画面

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,11 +1,13 @@
-import { memo, VFC } from 'react';
-import { Center, Spinner, Wrap, WrapItem } from '@chakra-ui/react';
+import { memo, useCallback, VFC } from 'react';
+import { Center, Spinner, useDisclosure, Wrap, WrapItem } from '@chakra-ui/react';
 import { UserCard } from '../organisms/user/UserCard';
 import { useAllUsers } from '../../hooks/useAllUsers';
 import { useEffect } from 'react';
+import { UserDetailModal } from '../organisms/user/UserDetailModal';
 
 export const UserManagement: VFC = memo(() => {
     const { getUsers, users, loading } = useAllUsers();
+    const { isOpen, onOpen, onClose } = useDisclosure();
     useEffect(
         () => {
             getUsers();
@@ -13,6 +15,15 @@ export const UserManagement: VFC = memo(() => {
         // eslint-disable-next-line
         [],
     );
+
+    const onClickUser = useCallback(
+        () => {
+            onOpen();
+        },
+        // eslint-disable-next-line
+        [],
+    );
+
     return (
         <>
             {loading ? (
@@ -23,11 +34,17 @@ export const UserManagement: VFC = memo(() => {
                 <Wrap p={{ base: 4, md: 10 }}>
                     {users.map((user) => (
                         <WrapItem key={user.id} mx="auto">
-                            <UserCard imageUrl="https://source.unsplash.com/random" userName={user.username} fullName={user.name} />
+                            <UserCard
+                                imageUrl="https://source.unsplash.com/random"
+                                userName={user.username}
+                                fullName={user.name}
+                                onClick={onClickUser}
+                            />
                         </WrapItem>
                     ))}
                 </Wrap>
             )}
+            <UserDetailModal isOpen={isOpen} onClose={onClose} />
         </>
     );
 });
